7b64c51a498a1d8f25eb87b7dde27c812c55423c
Jesus <espitaj26@gmail.com>
2021-12-09 04:16:30 -0800

    Add Windows menu item

--- src/menu.ts
+++ src/menu.ts
@@ -185,90 +185,113 @@ export default class MenuBuilder {
   buildDefaultTemplate() {
     const templateDefault = [
       {
-        label: '&File',
+        label: 'Opacity',
         submenu: [
           {
-            label: '&Open',
-            accelerator: 'Ctrl+O',
-          },
-          {
-            label: '&Close',
-            accelerator: 'Ctrl+W',
+            label: 'About Opacity',
+            //selector: 'orderFrontStandardAboutPanel:',
             click: () => {
-              this.mainWindow.close();
+              this.mainWindow.webContents.send('about-panel');
+            },
+          },
+          { type: 'separator' },
+          { label: 'Preferences', submenu: [{ label: 'Save Account Handle' }] },
+          { type: 'separator' },
+          {
+            label: 'Quit Opacity',
+            accelerator: 'Command+Q',
+            click: () => {
+              app.quit();
             },
           },
         ],
       },
       {
-        label: '&View',
-        submenu:
-          process.env.NODE_ENV === 'development' ||
-          process.env.DEBUG_PROD === 'true'
-            ? [
-                {
-                  label: '&Reload',
-                  accelerator: 'Ctrl+R',
-                  click: () => {
-                    this.mainWindow.webContents.reload();
-                  },
-                },
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  },
-                },
-                {
-                  label: 'Toggle &Developer Tools',
-                  accelerator: 'Alt+Ctrl+I',
-                  click: () => {
-                    this.mainWindow.webContents.toggleDevTools();
-                  },
-                },
-              ]
-            : [
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  },
-                },
-              ],
+        label: 'File',
+        submenu: [
+          {
+            label: 'New Folder',
+            //accelerator: 'Command+N',
+            click: () => {
+              this.mainWindow.webContents.send('new-folder');
+            },
+          },
+          {
+            label: 'Upload File(s)',
+            click: () => {
+              this.mainWindow.webContents.send('upload-file');
+            },
+          },
+          { type: 'separator' },
+          {
+            label: 'Upload Folder(s)',
+            click: () => {
+              this.mainWindow.webContents.send('upload-folder');
+            },
+          },
+          {
+            label: 'Share',
+            submenu: [{ label: 'Private Share' }, { label: 'Public Share' }],
+          },
+          {
+            label: 'Download',
+            click: () => {
+              this.mainWindow.webContents.send('download-file');
+            },
+          },
+          {
+            label: 'Rename',
+            click: () => {
+              this.mainWindow.webContents.send('rename-file');
+            },
+          },
+          {
+            label: 'Logout',
+            click: () => {
+              this.mainWindow.webContents.send('logout-app');
+            },
+          },
+        ],
+      },
+      {
+        label: 'Edit',
+        submenu: [
+          { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
+          { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
+          { type: 'separator' },
+          { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
+          { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
+          { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
+          {
+            label: 'Delete',
+          },
+        ],
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'as List',
+            click: () => {
+              this.mainWindow.webContents.reload();
+            },
+          },
+          {
+            label: 'as Gallery',
+            accelerator: 'Ctrl+Command+F',
+            click: () => {
+              this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+            },
+          },
+        ],
       },
       {
         label: 'Help',
         submenu: [
           {
-            label: 'Learn More',
+            label: 'Opacity Helper',
             click() {
-              shell.openExternal('https://electronjs.org');
-            },
-          },
-          {
-            label: 'Documentation',
-            click() {
-              shell.openExternal(
-                'https://github.com/electron/electron/tree/master/docs#readme'
-              );
-            },
-          },
-          {
-            label: 'Community Discussions',
-            click() {
-              shell.openExternal('https://www.electronjs.org/community');
-            },
-          },
-          {
-            label: 'Search Issues',
-            click() {
-              shell.openExternal('https://github.com/electron/electron/issues');
+              shell.openExternal('https://help.opacity.io/help');
             },
           },
         ],
