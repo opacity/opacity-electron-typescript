ccc5c88a6844ac5211f7c575b636a66f0e490a7d
Jesus <espitaj26@gmail.com>
2022-02-11 02:15:02 -0800

    # feature: searchbox ui add

--- src/components/Manager.tsx
+++ src/components/Manager.tsx
@@ -6,7 +6,13 @@ import streamsaver from 'streamsaver';
 import React, { useState, useEffect, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { Container, Table, ButtonToolbar, Spinner } from 'react-bootstrap';
+import {
+  Container,
+  Table,
+  ButtonToolbar,
+  Spinner,
+  Form,
+} from 'react-bootstrap';
 import { AiFillHome } from 'react-icons/ai';
 import Swal from 'sweetalert2';
 import Styled from 'styled-components';
@@ -69,7 +75,6 @@ const Checkbox = Styled.input.attrs({
 })``;
 
 let fileUploadingList: ProgressItem[] = [];
-let loadingFlagCnt = 0;
 let uploadingFileList = [];
 const THREAD_COUNT = 10;
 let curThreadNum = 0;
@@ -169,6 +174,8 @@ const Manager = () => {
     (path: string) => path.substr(0, path.lastIndexOf('/')),
     []
   );
+
+  const [searchname, SetSearchname] = React.useState('');
 
   // const isFileManaging = () => {
   //   setIsManaging(true);
@@ -838,6 +845,23 @@ const Manager = () => {
     changeAllCheckboxState(false);
   };
 
+  const FilterbyName = async (e) => {
+    console.log(searchname);
+    // e.stopPropagation();
+    // if (selectedFiles.length) {
+    //   setSelectedFiles([]);
+    // } else {
+    //   let fileMetaValues = fileMetaList;
+
+    //   if (!fileMetaList) {
+    //     await getFolderMetaList();
+    //     fileMetaValues = await getFileMetaList();
+    //   }
+
+    //   setSelectedFiles(fileMetaValues.filter((item) => "uploaded" in item));
+    // }
+  };
+
   const onDrop = React.useCallback(
     (files) => {
       selectFiles(files);
@@ -1104,6 +1128,22 @@ const Manager = () => {
               changeAllCheckboxState={changeAllCheckboxState}
             />
           </ButtonToolbar>
+          <div className="searchbox-container">
+            <Form.Control
+              className="searchbox-input"
+              placeholder="Search folder and files"
+              onChange={(e) => SetSearchname(e.target.value)}
+            />
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              onClick={FilterbyName}
+            >
+              <svg width="15px" height="15px">
+                <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
+              </svg>
+            </button>
+          </div>
           <Table size="sm" className="mt-2">
             <thead>
               <tr>
